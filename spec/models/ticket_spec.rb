3814d080f7d37b8f0617091fc691ab8af9845571
aksingh007 <aksingh9995@gmail.com>
2016-11-14 12:50:32 +0530

    Updated model spec

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,23 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    ticket1 = Ticket.create(:name => 'ticket1',:description => 'sample ticket1', :severity => 'Severity 1', :status => 'open')
+    ticket2 = Ticket.create(:name => 'ticket2',:description => 'sample ticket2', :severity => 'Severity 1', :status => 'open')
+    let(:ticket_ids) { [ticket1.id, ticket2.id] }
+  end
+  
+  describe "to_csv" do
 
-  describe "Importing data" do
-    let(:person) { Person.new }
-
-    let(:data) { "title\tsurname\tfirstname\t\rtitle2\tsurname2\tfirstname2\t\r"}
-    let(:result) {[["title","surname","firstname"],["title2","surname2","firstname2"]] }
+    let(:data) { "id\tname\tdescription\tcustomer_id\tassociate_id\tcreated_at\t\rid\tname2\tdescription2\tcustomer_id2\tassociate_id2\tcreated_a2t\t\r"}
+    let(:result) {[["Ticket_ID","Ticket Name","Ticket Description","Customer Username","Handled by Associate","Created AT"] }
 
     it "should read in the csv" do
       CSV.should_receive(:read).
         with("filename", :row_sep => "\r", :col_sep => "\t")
-      person.import("filename")
+      ticket.to_csv("filename")
     end
 
     it "should have result" do
       filename = mock(File, :exists? => true, :read => data)
-      person.import(filename).should eq(result)
+      ticket.to_csv(filename).should eq(result)
     end
   end
 end
